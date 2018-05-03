400d1b0f3f106ccf1dbb1153dbd435b22bf2a662
zxyango <zxyango@gmail.com>
2018-05-03 16:43:28 +0800

    init project show Hello world in web

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const chalk = require('chalk');
+const conf = require('./config/defaultConf');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello');
+});
+
+server.listen(conf.port, conf.hostname, () => {
+    const addr = `http://${conf.hostname}:${conf.port}`;
+    console.info(`Server started at ${chalk.green(addr)}`);
+});
